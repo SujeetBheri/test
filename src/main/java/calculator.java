5366791e0914a37165bb87f0880a6bfb22a11051
SujeetBheri <sujeetbheri@gmail.com>
2021-05-03 21:23:58 +0200

    commit

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -9,10 +9,6 @@ public class Calculator {
 	public int multiply(int a, int b) {
 		return a * b;
 	}
-	public int divide(int a, int b) {
-		return a \ b;
-	
-	}
 	public static void main{String[] arg}
 	{
 	}
